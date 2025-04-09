c3efd007e869dae2ecd1b86c18d9539e74cc4038
leonel1414 <leoo9211@hotmail.com>
2025-04-09 20:16:05 -0300

    Se elimina el link de especies de prueba

--- routers/especies.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controller = require("../controllers/especies.controller");
-
-// Ruta para registrar una especie
-router.post('/', controller.insertEspecie);
-
-module.exports = router;
