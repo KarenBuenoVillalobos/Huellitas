1b1c5642bdbb4f213fda436c311e1dbddbcf9949
leonel1414 <leoo9211@hotmail.com>
2025-07-09 13:24:16 -0300

    Modificaciones

--- db/db.js
+++ db/db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 //// CONEXION A LA BBDD ////
 const connection = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "",
-    database : "huellitas"
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
 });
 
 connection.connect((error) => {
