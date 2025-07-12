ddb455229e29d655e21eb1a2572f7de882acf1ee
KarenVillalobos98 <40954875@ifts24.edu.ar>
2025-07-11 21:59:57 -0300

    pop up para donaciones
    También se eliminaron cosas de la carpeta Borrador.

--- js/script.js
+++ js/script.js
@@ -29,7 +29,8 @@ let header = `
     ${isLogged ? '' : '<a href="/loginsesion">Iniciar Sesión</a>'}
     ${idRol === '1' ? '<a href="/admin" id="admin-link">Lista</a>' : ''}
     ${userInfo}
-    ${isLogged ? '<a href="#" id="logout-link"><img src="/img/icon-logout.png" alt="log-out"></a>' : ''}
+    ${isLogged ? '<a href="#" id="logout-link"><img src="/img/icon-exit.svg" alt="log-out" width="25"></a>' : ''}
+    <a href="#" id="notification-link"><img src="/img/icon-notificacion.svg" alt="icon-notificacion" class="icon-notificacion" width="25"></a>
     <label for="check" class="esconder-menu">&#215</label>
 </nav>
 `;
